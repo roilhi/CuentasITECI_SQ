c7c5ca5b48a3d303e04fdee7cc3418bf21c98c79
roilhi <roilhi@gmail.com>
2022-11-10 18:09:15 -0800

    Cambios en base de datos mongo, botones y textboxes, agregar matricula

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,23 +32,27 @@ namespace CuentasITECI_SQ
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.tbName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.tbemail = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.tbUser = new System.Windows.Forms.TextBox();
             this.tbPassword = new System.Windows.Forms.TextBox();
             this.btnBorrar = new System.Windows.Forms.Button();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbMat = new System.Windows.Forms.TextBox();
+            this.tbCarrera = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(736, 24);
+            this.pictureBox1.Location = new System.Drawing.Point(573, 269);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(141, 130);
+            this.pictureBox1.Size = new System.Drawing.Size(147, 136);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -59,24 +63,15 @@ namespace CuentasITECI_SQ
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(23, 79);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(258, 21);
+            this.label1.Size = new System.Drawing.Size(191, 21);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Nombre completo del estudiante:";
-            // 
-            // tbName
-            // 
-            this.tbName.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbName.Location = new System.Drawing.Point(287, 77);
-            this.tbName.Name = "tbName";
-            this.tbName.Size = new System.Drawing.Size(424, 27);
-            this.tbName.TabIndex = 2;
-            this.tbName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbName_KeyDown);
+            this.label1.Text = "Apellidos del estudiante:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(23, 133);
+            this.label2.Location = new System.Drawing.Point(23, 226);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(244, 21);
             this.label2.TabIndex = 3;
@@ -85,50 +80,33 @@ namespace CuentasITECI_SQ
             // tbemail
             // 
             this.tbemail.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbemail.Location = new System.Drawing.Point(287, 131);
+            this.tbemail.Location = new System.Drawing.Point(273, 226);
             this.tbemail.Name = "tbemail";
-            this.tbemail.Size = new System.Drawing.Size(424, 27);
+            this.tbemail.Size = new System.Drawing.Size(447, 27);
             this.tbemail.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(455, 193);
+            this.label3.Location = new System.Drawing.Point(171, 323);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(96, 21);
             this.label3.TabIndex = 5;
             this.label3.Text = "Contraseña:";
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(23, 193);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(156, 21);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Usuario Aula Virtual:";
-            // 
-            // tbUser
-            // 
-            this.tbUser.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbUser.Location = new System.Drawing.Point(185, 191);
-            this.tbUser.Name = "tbUser";
-            this.tbUser.Size = new System.Drawing.Size(247, 27);
-            this.tbUser.TabIndex = 7;
-            // 
             // tbPassword
             // 
             this.tbPassword.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbPassword.Location = new System.Drawing.Point(557, 191);
+            this.tbPassword.Location = new System.Drawing.Point(273, 323);
             this.tbPassword.Name = "tbPassword";
             this.tbPassword.Size = new System.Drawing.Size(247, 27);
             this.tbPassword.TabIndex = 8;
             // 
             // btnBorrar
             // 
-            this.btnBorrar.Location = new System.Drawing.Point(684, 241);
+            this.btnBorrar.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.Location = new System.Drawing.Point(273, 374);
             this.btnBorrar.Name = "btnBorrar";
             this.btnBorrar.Size = new System.Drawing.Size(120, 33);
             this.btnBorrar.TabIndex = 9;
@@ -136,24 +114,92 @@ namespace CuentasITECI_SQ
             this.btnBorrar.UseVisualStyleBackColor = true;
             this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
             // 
+            // tbLastName
+            // 
+            this.tbLastName.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbLastName.Location = new System.Drawing.Point(229, 77);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(424, 27);
+            this.tbLastName.TabIndex = 10;
+            this.tbLastName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbLastName_KeyDown);
+            // 
+            // tbName
+            // 
+            this.tbName.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbName.Location = new System.Drawing.Point(229, 127);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(424, 27);
+            this.tbName.TabIndex = 11;
+            this.tbName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbName_KeyDown);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(23, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(200, 21);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Nombre(s) del estudiante:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(130, 271);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(137, 21);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Matrícula interna:";
+            // 
+            // tbMat
+            // 
+            this.tbMat.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbMat.Location = new System.Drawing.Point(273, 269);
+            this.tbMat.Name = "tbMat";
+            this.tbMat.Size = new System.Drawing.Size(247, 27);
+            this.tbMat.TabIndex = 14;
+            // 
+            // tbCarrera
+            // 
+            this.tbCarrera.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCarrera.Location = new System.Drawing.Point(229, 179);
+            this.tbCarrera.Name = "tbCarrera";
+            this.tbCarrera.Size = new System.Drawing.Size(491, 27);
+            this.tbCarrera.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(147, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 21);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Carrera:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(897, 296);
+            this.ClientSize = new System.Drawing.Size(786, 430);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbCarrera);
+            this.Controls.Add(this.tbMat);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.tbLastName);
             this.Controls.Add(this.btnBorrar);
             this.Controls.Add(this.tbPassword);
-            this.Controls.Add(this.tbUser);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.tbemail);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.tbName);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Acceso a Servicios Digitales ";
+            this.Text = "Acceso a Servicios Digitales UNIENS San Quintín";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -165,14 +211,18 @@ namespace CuentasITECI_SQ
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox tbName;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox tbemail;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox tbUser;
         private System.Windows.Forms.TextBox tbPassword;
         private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbMat;
+        private System.Windows.Forms.TextBox tbCarrera;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -11,6 +11,8 @@ using MetroFramework.Forms;
 using MetroFramework;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using System.Xml.Linq;
 
 namespace CuentasITECI_SQ
 {
@@ -23,45 +25,111 @@ namespace CuentasITECI_SQ
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            tbName.Select();
-            tbName.Focus();
+            MessageBox.Show("Ingresa los apellidos del estudiante");
+            tbLastName.Select();
+            tbLastName.Focus();
         }
 
-        private void tbName_KeyDown(object sender, KeyEventArgs e) 
+        private string ClaveCarrera(string clave)
         {
-            if (e.KeyCode == Keys.Enter) 
+            string Carrera = "";
+            if (clave == "LAITE")
             {
-                tbName.Text = tbName.Text.Trim().ToUpper();
-                var settings = MongoClientSettings.FromConnectionString("mongodb+srv://itecidb2:iteci2021@clusteriteci.rnxhk.mongodb.net/Prepa_ITECI_Ens?connect=replicaSet");
-                //var settings = MongoClientSettings.FromConnectionString("mongodb+srv://itecidb:iteci2021@clusteriteci.rnxhk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-                var client = new MongoClient(settings);
-                var database = client.GetDatabase("UNIENS_Campus_SQ");
-                var collection = database.GetCollection<BsonDocument>("Mails_Estudiantes");
-                var filter = Builders<BsonDocument>.Filter.Eq("WholeName", tbName.Text.Trim().ToUpper());
-                var BsonDoc = collection.Find(filter).FirstOrDefault();
-                try 
-                {
-                    string MoodleUser = BsonDoc["MoodleUser"].AsString;
-                    tbUser.Text = MoodleUser;
-                    string password = BsonDoc["Password"].AsString;
-                    tbPassword.Text = password;
-                    string email = BsonDoc["email"].AsString;
-                    tbemail.Text = email;
-                }
-                catch
-                {
-                    MetroMessageBox.Show(this, "El alumno no existe en los registros de la base de datos o el nombre fue escrito incorrectamente", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-        
+                Carrera = "LICENCIADO EN ADMINISTRACIÓN DE EMPRESAS Y LOGÍSTICA";
+            }
+            else if (clave == "LCEITE")
+            {
+                Carrera = "LICENCIADO EN CIENCIAS DE LA EDUCACION";
+            }
+            else if (clave == "IDITE")
+            {
+                Carrera = "INGENIERIA INDUSTRIAL";
+            }
+            else if (clave == "LDITE")
+            {
+                Carrera = "LICENCIADO EN DERECHO";
+            }
+            else if (clave == "LFITE")
+            {
+                Carrera = "LICENCIADO EN FILOSOFÍA";
+            }
+            else if (clave == "LMAITE")
+            {
+                Carrera = "LICENCIADO EN MEDIOS AUDIOVISUALES";
+            }
+            else if (clave == "LNITE")
+            {
+                Carrera = "LICENCIADO EN NEGOCIOS INTERNACIONALES";
+            }
+            else if (clave == "LCRITE")
+            {
+                Carrera = "LICENCIADO EN CRIMINOLOGÍA";
+            }
+            return Carrera;
+        }
+        private void tbLastName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                tbLastName.Text = tbLastName.Text.Trim().ToUpper();
+                MessageBox.Show("Ingresa los nombres del estudiante");
+                tbName.Select();
+                tbName.Focus();
             }
         }
+        private void tbName_KeyDown(object sender, KeyEventArgs e) 
+                {
+                    if (e.KeyCode == Keys.Enter) 
+                    {
+                        tbName.Text = tbName.Text.Trim().ToUpper();
+                        // cluster mongo sistemas UNIENS
+                        // mongodb+srv://uniens:uniens22@cluster0.ex5nn.mongodb.net/?retryWrites=true&w=majority?connect=replicaSet
+                        // var settings = MongoClientSettings.FromConnectionString("mongodb+srv://itecidb2:iteci2021@clusteriteci.rnxhk.mongodb.net/Prepa_ITECI_Ens?connect=replicaSet");
+                        string wName = tbLastName.Text + " " + tbName.Text;
+                        var settings = MongoClientSettings.FromConnectionString("mongodb+srv://uniens:uniens22@cluster0.ex5nn.mongodb.net/?retryWrites=true&w=majority?connect=replicaSet");
+                        var client = new MongoClient(settings);
+                        var database = client.GetDatabase("studSQ");
+                        var collection = database.GetCollection<BsonDocument>("infoMails");
+                        var filter = Builders<BsonDocument>.Filter.Eq("wholeName", wName);
+                        var BsonDoc = collection.Find(filter).FirstOrDefault();
+               try
+                {
+                    //string MoodleUser = BsonDoc["MoodleUser"].AsString;
+                    //tbUser.Text = MoodleUser;
+                    if (BsonDoc != null)
+                    {
+                        string password = BsonDoc["password"].AsString;
+                        tbPassword.Text = password;
+                        string email = BsonDoc["mail"].AsString;
+                        tbemail.Text = email;
+                        string matInt = BsonDoc["matInt"].AsString;
+                        tbMat.Text = matInt;
+                        string clave = BsonDoc["carrera"].AsString;
+                        tbCarrera.Text = ClaveCarrera(clave);
+                    }
+                    else 
+                    {
+                        MetroMessageBox.Show(this, "El usuario no existe, favor de verificar", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
 
+                        }
+                        catch
+                        {
+                            MetroMessageBox.Show(this, "Error en la conexión con la base de datos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+
+                    }
+                }
+        
         private void btnBorrar_Click(object sender, EventArgs e)
         {
+            tbLastName.Text = " ";
             tbName.Text = " ";
             tbemail.Text = " ";
             tbPassword.Text = " ";
-            tbUser.Text = " ";
+            tbMat.Text = " ";
+            tbCarrera.Text = " ";
+            tbMat.Text = " ";
         }
     }
 }
